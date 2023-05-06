d4fa1ce5961c9cd20e9686dd981176897ba7d9b7
Szymon <67510803+Szymoncz@users.noreply.github.com>
2023-05-06 16:37:28 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,22 @@
+/*
+Autor: Szymon Czudowski - s26858
+Pomoc: stack overflow
+Zadanie 4:
+Zadaniem jest stworzenie klasy symulującej działanie sejfu. Użytkownik by otworzyć sejf musi podać pin. W wypadku podania błędnego pinu do sejfu, zostanie wszczęty alarm. Działania podjęte w wyniku uruchomienia alarmu nie zależą od wykonawców sejfu. Co się stanie w odpowiedzi na wprowadzenie błędnego pinu zależy, od właściciela sejfu i zasobności jego portfela. Może on wynająć firmę ochroniarską, spuścić kratę, wypuścić psy itp. Sejf powinien akceptować dowolne działania spełniające założenia wyrażone w interfejsie Alarm.
+
+Każdy dostawca urządzenia alarmowego pozwala do niego podłączyć dowolne urządzenie rejestrujące zdarzenia, tzw. Logger. Logger uwzględnia: - poziom ważności zdarzenia: Severity - źródło zdarzenia: EventSource - komunikat typu String
+
+Zadanie 1 (4 punkty)
+Zaimplementuj klasę Safe wraz z wymaganymi klasami Alarm i PinEvent, bez systemu rejestracji zdarzeń.
+
+Zadanie 2 (4 punkty)
+Do implementacji z zadania 1 dodaj opisany system rejestracji zdarzeń wraz z przykładowymi implementacjami klas FileLogger oraz ConsoleLogger.
+
+Zadanie 3 (2 punkty)
+Przeprowadź testy poprawności działania sejfu uwzględniając różne konfiguracje alarmów i urządzeń rejestrujących.
+
+*/
+
 package alarmPackages;
 public class Main {
     public static void main(String[] args) {
